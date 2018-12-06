86db539ff284312d07a467acbb96cd8cd9f585db
Will Godfrey <w.gowman.godfrey@gmail.com>
2018-12-06 10:35:35 -0800

    c2 - configure webpack

---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, './client/index.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, './public'),
+  },
+  resolve: {
+    extensions: ['.js','.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: path.join(__dirname, '/client'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react', 'stage-0'],
+        },
+      },
+      {
+        test: /\.css$/,
+        include: path.join(__dirname, '/client'),
+        use: [
+          { loader: 'style-loader'},
+          { loader: 'css-loader'},
+        ],
+      },
+    ],
+  },
+};
